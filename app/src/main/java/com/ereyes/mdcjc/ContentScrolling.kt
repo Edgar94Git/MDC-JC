190812aefc15dcfe95d9edbc8c228786d4ebf9ab
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2023-04-08 12:49:38 -0600

    add image and resources dimens

--- app/src/main/java/com/ereyes/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/ereyes/mdcjc/ContentScrolling.kt
@@ -1,10 +1,8 @@
 package com.ereyes.mdcjc
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
@@ -13,6 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ereyes.mdcjc.ui.theme.MDCJCTheme
@@ -43,7 +44,14 @@ fun Content(modifier: Modifier = Modifier) {
                 .padding(8.dp)
         ) {
             Column {
-
+                Image(
+                    painter = painterResource(id = R.drawable.ic_shop),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(dimensionResource(id = R.dimen.card_imf_cover_heigth))
+                        .background(colorResource(id = R.color.teal_200))
+                )
             }
         }
     }
