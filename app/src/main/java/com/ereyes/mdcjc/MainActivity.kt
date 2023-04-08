fd5b66e8e041e225e39c0d399756619961298d26
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2023-04-08 11:50:45 -0600

    add bottomAppBar

--- app/src/main/java/com/ereyes/mdcjc/MainActivity.kt
+++ app/src/main/java/com/ereyes/mdcjc/MainActivity.kt
@@ -3,13 +3,15 @@ package com.ereyes.mdcjc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ereyes.mdcjc.ui.theme.MDCJCTheme
@@ -30,7 +32,20 @@ class MainActivity : ComponentActivity() {
 fun ContentMain(){
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        backgroundColor = MaterialTheme.colors.background
+        backgroundColor = MaterialTheme.colors.background,
+        bottomBar = {
+            BottomAppBar {
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                    }
+                }
+                Spacer(modifier = Modifier.weight(1f, true))
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.MoreVert, contentDescription = "Option")
+                }
+            }
+        }
     ) { padding ->
         Content(Modifier.padding(padding))
     }
