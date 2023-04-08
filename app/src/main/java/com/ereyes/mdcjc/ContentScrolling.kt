f2ed7a26e2c9efcce581b5216c26ac125c7605b8
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2023-04-08 12:37:44 -0600

    add card

--- app/src/main/java/com/ereyes/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/ereyes/mdcjc/ContentScrolling.kt
@@ -3,9 +3,11 @@ package com.ereyes.mdcjc
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberUpdatedState
@@ -31,7 +33,18 @@ fun ContentPreview() {
 
 @Composable
 fun Content(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
-        
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Column {
+
+            }
+        }
     }
 }
